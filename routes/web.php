1fabe50908558c8c8635dc7ea74c3f0d0a42c0df
assaup <samir.assaup@mail.ru>
2025-10-30 18:10:38 +0300

    lab2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, 'index']);
+Route::get('/full_image/{img}', [MainController::class, 'show']);
+
+Route::get('/about', function(){
+    return view('main.about');
 });
-Route::get('/main', function () {
-    return view('layout');
-});
-Route::get('/about', function () {
-    return view('main/about');
-});
+
 Route::get('/contact', function () {
     $array = [
         'name'=>'Moscow Polytech',
